84c465245b0b7667c14552a1c489027b67c25b4d
aeschylus <scipioaffricanus@gmail.com>
2015-05-12 12:32:39 -0700

    first commit with node module scaffolding

---
+++ example/simple.js
@@ -0,0 +1,13 @@
+/*
+ * manifesto-client
+ * ient.github.io
+ *
+ * Copyright (c) 2015 Drew Winget
+ * Licensed under the Apache 2.0 license.
+ */
+
+'use strict';
+
+var manifestoClient = require('../');
+
+console.log(manifestoClient()); // "awesome"
---
+++ test/manifesto-client_test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var manifestoClient = require('../');
+var assert = require('should');
+
+describe('manifestoClient', function () {
+
+  it('should be awesome', function () {
+    manifestoClient().should.equal('awesome');
+  });
+
+});
